c20d2f7d91e42a85ef325ef11049dac995b64191
Melissa Lawrence <mlawrence537@gmail.com>
2021-08-20 12:51:04 -0400

    add Routes, add entry details component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './index.css';
 import { users, dream_journals, entries } from './data.js';
 import User from './User';
 import DreamJournal from './DreamJournal';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   
   return (
     <div className="App">
-      <User users={users} />
-      <DreamJournal dream_journals={dream_journals} entries={entries}/>
+      <Switch>
+        <Route exact path='/profile'>
+          <User users={users} />
+          <DreamJournal dream_journals={dream_journals} entries={entries}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
