8df37d385469998d110df79e181c0d555457ab51
Melissa Lawrence <mlawrence537@gmail.com>
2021-08-25 13:57:22 -0400

    center all entry detail page content

--- src/EntryDetails.js
+++ src/EntryDetails.js
@@ -7,10 +7,9 @@ function EntryDetails() {
   
     return (
       <div>
-        <h1>{currentEntry.name}</h1>
-        <p>{currentEntry.content}</p>
-        <canvas id="test-canvas-1" width="300" height="200">
-        </canvas>
+        <h1 className="entryDetailHeader">{currentEntry.name}</h1>
+        <p className="entryDetailContent">{currentEntry.content}</p>
+        <canvas id="test-canvas-1" width="300" height="200" />
       </div>
     );
   }
