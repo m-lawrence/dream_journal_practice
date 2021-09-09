52c1201025b1d1a92d612c667c17666da3019e82
Melissa Lawrence <mlawrence537@gmail.com>
2021-09-09 13:56:19 -0400

    implement useRef and getContext

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,12 +1,21 @@
 import React, { useRef } from 'react';
 
 function Canvas({ currentEntry }) {
+    const canvasRef = useRef(null)
   
     return (
       <div>
         <h1 className="entryDetailHeader">{currentEntry.name}</h1>
         <p className="entryDetailContent">{currentEntry.content}</p>
-        <canvas id="test-canvas-1" width="300" height="200" onClick={e => {alert(e.clientX)}}/>
+        <canvas 
+            ref={canvasRef}
+            id="test-canvas-1" 
+            width="300" 
+            height="200" 
+            onClick={e => {
+                const canvas = canvasRef.current
+                const ctx = canvas.getContext('2d')
+                }}/>
       </div>
     );
   }
